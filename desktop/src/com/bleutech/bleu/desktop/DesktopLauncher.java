68d926a82851b82bcdee08699871f3e15f8cf809
mcprog <michaelcurtis96@gmail.com>
2014-10-24 07:25:55 -0700

    initial commit

---
+++ desktop/src/com/bleutech/bleu/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bleutech.bleu.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bleutech.bleu.Bleu;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Bleu(), config);
+	}
+}
